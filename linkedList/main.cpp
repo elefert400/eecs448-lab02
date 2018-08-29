eb26ce6f060e3340ee5c274c3134dbfb2c702284
Ethan <elefert400@gmail.com>
2018-08-29 09:59:21 -0500

    size method working

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -7,12 +7,16 @@
 #include <iostream>
 #include "LinkedList.h"
 #include "Test_LinkedList.h"
+using namespace std;
 
 int main(int argc, char** argv)
 {
 	const int TEST_SIZE = 50;
 	Test_LinkedList tester(TEST_SIZE);
+	int i;
+	
 	tester.runTests();
+	cin >> i;
 	
 	return (0);
 
